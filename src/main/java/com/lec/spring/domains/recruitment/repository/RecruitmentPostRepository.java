49003c0f6f64b96c6b4bcd7ecc77319de9bc1ebe
kmk23e <010nho5959@gmail.com>
2025-01-24 14:02:55 +0900

    Feat(#3): RecruitmentPostRepository 수정

--- src/main/java/com/lec/spring/domains/recruitment/repository/RecruitmentPostRepository.java
+++ src/main/java/com/lec/spring/domains/recruitment/repository/RecruitmentPostRepository.java
@@ -1,4 +1,17 @@
 package com.lec.spring.domains.recruitment.repository;
 
+import com.lec.spring.domains.recruitment.entity.RecruitmentPost;
+
+import java.util.List;
+
 public interface RecruitmentPostRepository {
+    // 모집글 조회 (페이징+카테고리)
+    List<RecruitmentPost> list();
+
+    // 모집글 적기
+    RecruitmentPost write(RecruitmentPost recruitmentPost);
+
+    // 모집글 수정
+
+    // 모집글 삭제
 }
