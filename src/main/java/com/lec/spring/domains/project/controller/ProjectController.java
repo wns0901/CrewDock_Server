9e59dba434e4d216d78e802c8fd31ddd12f02517
Minhyeok-Create <drawingself7@gmail.com>
2025-01-24 15:26:13 +0900

    Feat(#1): Git데이터 호출 컨트롤러 작성

--- src/main/java/com/lec/spring/domains/project/controller/ProjectController.java
+++ src/main/java/com/lec/spring/domains/project/controller/ProjectController.java
@@ -1,4 +1,35 @@
 package com.lec.spring.domains.project.controller;
 
+import com.lec.spring.domains.project.dto.GitDataDTO;
+import com.lec.spring.domains.project.service.GitService;
+import com.lec.spring.domains.project.util.GitUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
 public class ProjectController {
+
+    private final GitService gitService;
+
+    @GetMapping("/projects/{projectId}/githubs")
+    public Mono<ResponseEntity<GitDataDTO>> getGitData(@PathVariable Long projectId, @RequestParam("gitURL") String gitUrl) {
+
+        String[] ownerAndRepo;
+        try{
+            ownerAndRepo = GitUtil.extractOwnerAndRepo(gitUrl);
+        } catch (IllegalArgumentException e) {
+            return Mono.just(ResponseEntity.badRequest().body(null));
+        }
+        String owner = ownerAndRepo[0];
+        String repo = ownerAndRepo[1];
+        return gitService.getGitHubData(owner,repo)
+                .map(gitDataDTO -> ResponseEntity.ok().body(gitDataDTO))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
