db627763211c38fa1131ed41f9960177e51513ec
0105moonsun <0105moonsun@gmail.com>
2025-01-24 14:31:03 +0900

    Feat(#4): ãservice 초기 구현

--- src/main/java/com/lec/spring/domains/portfolio/service/PortfolioServiceImpl.java
+++ src/main/java/com/lec/spring/domains/portfolio/service/PortfolioServiceImpl.java
@@ -1,4 +1,47 @@
 package com.lec.spring.domains.portfolio.service;
 
-public class PortfolioServiceImpl {
+import com.lec.spring.domains.portfolio.entity.Portfolio;
+import com.lec.spring.domains.portfolio.repository.PortfolioRepository;
+import com.lec.spring.domains.portfolio.repository.PortfolioStackRepository;
+
+import java.util.List;
+
+public class PortfolioServiceImpl implements PortfolioService {
+    private PortfolioRepository portfolioRepository;
+    private PortfolioStackRepository portfolioStackRepository;
+
+    public PortfolioServiceImpl(PortfolioRepository portfolioRepository, PortfolioStackRepository portfolioStackRepository) {
+        this.portfolioRepository = portfolioRepository;
+        this.portfolioStackRepository = portfolioStackRepository;
+    }
+
+    @Override
+    public List<Portfolio> getUserPortfolios(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Portfolio> getPortfoliosWithStacks(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Portfolio> getUserPortfoliosWithLimit(Long userId, int row) {
+        return List.of();
+    }
+
+    @Override
+    public Portfolio createPortfolio(Portfolio portfolio) {
+        return null;
+    }
+
+    @Override
+    public Portfolio updatePortfolio(Portfolio portfolio, List<Long> stackIds) {
+        return null;
+    }
+
+    @Override
+    public void deletePortfolio(Long userId) {
+
+    }
 }
