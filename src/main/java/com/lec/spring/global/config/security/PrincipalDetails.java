f2a787a85d402d8e25b2a8bae3d7f80f3bb66f25
wns0901 <wnswns012@gmail.com>
2025-01-24 15:21:33 +0900

    Feat(#2): userRepository 테스트

---
+++ src/main/java/com/lec/spring/global/config/security/PrincipalDetails.java
@@ -0,0 +1,83 @@
+package com.lec.spring.global.config.security;
+
+import com.lec.spring.domains.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private User user;
+
+    private Map<String, Object> attributes;
+
+    public User getUser() {
+        return user;
+    }
+
+    // 일반 로그인 (username, password) 용 생성자
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    // OAuth 인증용 생성자
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        // TODO : 권한 정보를 가져와서 collection 에 추가
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
---
+++ src/test/java/com/lec/spring/domains/user/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.lec.spring.domains.user.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+}
