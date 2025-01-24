5e47f10e13cf82887b469a0b5d2cd8105b7fdebf
0105moonsun <0105moonsun@gmail.com>
2025-01-24 12:33:54 +0900

    Feat(#4): portfolioService 기(인터페이스) UD 생성

--- src/main/java/com/lec/spring/domains/portfolio/service/PortfolioService.java
+++ src/main/java/com/lec/spring/domains/portfolio/service/PortfolioService.java
@@ -1,4 +1,26 @@
 package com.lec.spring.domains.portfolio.service;
 
+import com.lec.spring.domains.portfolio.entity.Portfolio;
+
+import java.util.List;
+
 public interface PortfolioService {
+    // 유저의 포트폴리오 조회 (row 제한 없음)
+    List<Portfolio> getUserPortfolios(Long userId);
+    // 특정 유저의 포트폴리오와 스택을 조회
+    List<Portfolio> getPortfoliosWithStacks(Long userId);
+
+    // 유저의 포트폴리오 조회 (row 제한 있음)
+    List<Portfolio> getUserPortfoliosWithLimit(Long userId, int row);
+
+    // 유저의 포트폴리오 작성
+    Portfolio createPortfolio(Portfolio portfolio);
+
+    // 유저 포트폴리오 수정
+    Portfolio updatePortfolio(Portfolio portfolio, List<Long> stackIds);
+
+    // 유저 포트폴리오 삭제
+    void deletePortfolio(Long userId);
+
+
 }
---
+++ src/main/java/com/lec/spring/domains/portfolio/service/PortfolioServiceImpl.java
@@ -0,0 +1,4 @@
+package com.lec.spring.domains.portfolio.service;
+
+public class PortfolioServiceImpl {
+}
