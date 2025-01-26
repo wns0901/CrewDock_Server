23dde055eed6b7fdd2b66e6487d0c7f279c7d0b0
Minhyeok-Create <drawingself7@gmail.com>
2025-01-26 11:27:14 +0900

    Feat(#14): 프로젝트 멤버 권한, 탈퇴 로직

--- src/main/java/com/lec/spring/domains/project/service/ProjectMemberServiceImpl.java
+++ src/main/java/com/lec/spring/domains/project/service/ProjectMemberServiceImpl.java
@@ -1,12 +1,13 @@
 package com.lec.spring.domains.project.service;
 
-import com.lec.spring.domains.project.entity.QProject;
-import com.lec.spring.domains.project.entity.QProjectMember;
+import com.lec.spring.domains.project.entity.*;
 import com.lec.spring.domains.user.entity.QUser;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,5 +27,31 @@ public class ProjectMemberServiceImpl {
                 .join(projectMember.user, user)
                 .where(project.id.eq(projectId))
                 .fetch();
+    }
+    // 탈퇴
+    @Transactional
+    public void updateMemberStatus(Long projectId, Long userId, ProjectMemberStatus status) {
+        QProjectMember projectMember = QProjectMember.projectMember;
+
+        // 상태 변경 로직
+        queryFactory.update(projectMember)
+                .set(projectMember.status, status)
+                .where(
+                        projectMember.project.id.eq(projectId)
+                                .and(projectMember.user.id.eq(userId))
+                )
+                .execute();
+    }
+    @Transactional
+    public void updateMemberAuthority(Long projectId, Long userId, ProjectMemberAuthirity authority) {
+        QProjectMember projectMember = QProjectMember.projectMember;
+        long updatesuccess = queryFactory.update(projectMember)
+                .set(projectMember.authority, authority)
+                .where(projectMember.project.id.eq(projectId)
+                        .and(projectMember.user.id.eq(userId)))
+                .execute();
+        if (updatesuccess == 0) {
+            throw new EntityNotFoundException("존재하지 않은 멤버임");
+        }
     }
 }
