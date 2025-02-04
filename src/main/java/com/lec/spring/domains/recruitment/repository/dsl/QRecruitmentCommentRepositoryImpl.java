6b473ffb605ba2550c6b3779967c47a65e88a905
kmk23e <010nho5959@gmail.com>
2025-02-04 15:12:10 +0900

    Feat(#3): RecruitmentCommentService 구현2

--- src/main/java/com/lec/spring/domains/recruitment/repository/dsl/QRecruitmentCommentRepositoryImpl.java
+++ src/main/java/com/lec/spring/domains/recruitment/repository/dsl/QRecruitmentCommentRepositoryImpl.java
@@ -1,17 +1,36 @@
 package com.lec.spring.domains.recruitment.repository.dsl;
 
+import com.lec.spring.domains.recruitment.entity.QRecruitmentComment;
 import com.lec.spring.domains.recruitment.entity.RecruitmentComment;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import java.util.List;
 
 public class QRecruitmentCommentRepositoryImpl implements QRecruitmentCommentRepository {
+    private final JPAQueryFactory queryFactory;
 
-    // 부모가 있다면 공간을 조금 빼서
+    public QRecruitmentCommentRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+    // 구현하려는 것. 부모가 있는지 없는지 확인하려고
 
+    // 부모가 있다면 계단식으로
+    //   나 부모댓글
+    //      나 자식댓글
     @Override
     public List<RecruitmentComment> Comment(List<RecruitmentComment> comments) {
+
+        QRecruitmentComment comment = QRecruitmentComment.recruitmentComment;
+        // 생각없이 코딩침...
+
+//        BooleanExpression condition =
         return List.of();
     }
 
+    private BooleanExpression commentFilter (RecruitmentComment comment) {
+
+        return null;
+    }
 
 }
