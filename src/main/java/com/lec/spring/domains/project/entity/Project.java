1c52e20881868018c14e96cb0c1906926cde3766
solsol-815 <solsolsol815815@gmail.com>
2025-02-07 10:03:32 +0900

    Conflict(): merge

--- src/main/java/com/lec/spring/domains/project/entity/Project.java
+++ src/main/java/com/lec/spring/domains/project/entity/Project.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/lec/spring/domains/user/entity/User.java
+++ src/main/java/com/lec/spring/domains/user/entity/User.java
@@ -15,6 +15,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
