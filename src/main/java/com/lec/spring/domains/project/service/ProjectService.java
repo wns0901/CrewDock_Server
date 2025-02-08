519370f3e40b2a4653376b2854ffd99cb9f91071
Minhyeok-Create <drawingself7@gmail.com>
2025-02-08 18:56:20 +0900

    Feat(#1) 프로젝트 수정 로직 복구작업

--- src/main/java/com/lec/spring/domains/project/service/ProjectService.java
+++ src/main/java/com/lec/spring/domains/project/service/ProjectService.java
@@ -1,5 +1,6 @@
 package com.lec.spring.domains.project.service;
 
+import com.lec.spring.domains.project.dto.ProjectUpdateDTO;
 import com.lec.spring.domains.project.entity.Project;
 
 import java.util.Calendar;
@@ -8,5 +9,5 @@ import java.util.List;
 public interface ProjectService {
     Project getProject(Long projectId);
 
-    void updateProject(Long projectId, Project updatedProject);
+    void updateProject(Long projectId, ProjectUpdateDTO updatedProject);
 }
