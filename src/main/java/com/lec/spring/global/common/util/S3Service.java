07f2a681287c245d9439d86b8de6c6b4c18ca562
wns0901 <wnswns012@gmail.com>
2025-02-11 00:08:49 +0900

    Feat(#33): s3 기능 구현

--- src/main/java/com/lec/spring/global/common/util/S3Service.java
+++ src/main/java/com/lec/spring/global/common/util/S3Service.java
@@ -1,4 +1,11 @@
 package com.lec.spring.global.common.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface S3Service {
+    String uploadFile(MultipartFile file, BucketDirectory bucketDirectory);
+
+    void deleteFile(String url);
 }
