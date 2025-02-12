f5d677124b62430b65c6111f44c22170303ded08
sihooh <khy2001smy@gmail.com>
2025-02-12 17:51:27 +0900

    Feat(#5): 상세보기 url 수정, ProjectIssue -> ProjectIssueDTO로 변경

--- src/main/java/com/lec/spring/domains/project/service/ProjectIssueService.java
+++ src/main/java/com/lec/spring/domains/project/service/ProjectIssueService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface ProjectIssueService {
 
     // 이슈 작성 (작업명, 담당자, 상태, 우선순위, 타임라인)
-    ProjectIssue save(Long projectId, ProjectIssueDTO projectIssueDTO);
+    ProjectIssueDTO save(Long projectId, ProjectIssueDTO projectIssueDTO);
 
     // 해당 프로젝트의 모든 이슈 목록 출력
     List<ProjectIssueDTO> listByProjectId(Long projectId);
@@ -20,10 +20,10 @@ public interface ProjectIssueService {
     ProjectIssueDTO getIssueDetail (Long projectId, Long issueId);
 
     // 특정 이슈들 삭제하기 -> 다중 삭제 기능
-    int deleteByIds(List<Long> issueIds);
+    int deleteByIds(Long projectId, List<Long> issueIds);
 
     // 특정 이슈 삭제 -> 개별 삭제
-    int deleteById(Long issueId);
+    int deleteById(Long projectId, Long issueId);
 
 
 }
